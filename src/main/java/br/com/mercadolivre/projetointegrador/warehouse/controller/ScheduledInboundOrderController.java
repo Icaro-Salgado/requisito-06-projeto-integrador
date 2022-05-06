ca51f47832068051fc6df607874505ad484e7a2f
Icaro Gabriel Pereira Salgado <icaro.salgado@mercadolivre.com>
2022-05-05 21:12:11 -0300

    feat: adding swagger

--- src/main/java/br/com/mercadolivre/projetointegrador/warehouse/controller/ScheduledInboundOrderController.java
+++ src/main/java/br/com/mercadolivre/projetointegrador/warehouse/controller/ScheduledInboundOrderController.java
@@ -1,14 +1,21 @@
 package br.com.mercadolivre.projetointegrador.warehouse.controller;
 
 import br.com.mercadolivre.projetointegrador.warehouse.assembler.ScheduledInboundOrderAssembler;
+import br.com.mercadolivre.projetointegrador.warehouse.dto.request.CreateWarehousePayloadDTO;
 import br.com.mercadolivre.projetointegrador.warehouse.dto.request.ScheduledInboundOrderDTO;
 import br.com.mercadolivre.projetointegrador.warehouse.dto.response.ResponseScheduledInboundOrderDTO;
+import br.com.mercadolivre.projetointegrador.warehouse.exception.ErrorDTO;
 import br.com.mercadolivre.projetointegrador.warehouse.mapper.ScheduledInboundOrderMapper;
 import br.com.mercadolivre.projetointegrador.warehouse.model.ScheduledInboundOrder;
 import br.com.mercadolivre.projetointegrador.warehouse.service.ProductService;
 import br.com.mercadolivre.projetointegrador.warehouse.service.ScheduledInboundOrderService;
 import br.com.mercadolivre.projetointegrador.warehouse.view.ScheduledInboundOrderView;
 import com.fasterxml.jackson.annotation.JsonView;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.format.annotation.DateTimeFormat;
@@ -32,6 +39,18 @@ public class ScheduledInboundOrderController {
     private final ScheduledInboundOrderMapper scheduledInboundOrderMapper;
     private final ScheduledInboundOrderAssembler scheduledInboundOrderAssembler;
 
+    @Operation(summary = "CRIA UM NOVO INBOUND ORDER AGENDADO", description = "Cria um novo inbound order agendado")
+    @ApiResponses(
+            value = {
+                    @ApiResponse(
+                            responseCode = "201",
+                            description = "ScheduledInboundOrder criado com sucesso",
+                            content = {
+                                    @Content(
+                                            mediaType = "application/json",
+                                            schema = @Schema(implementation = ResponseScheduledInboundOrderDTO.class))
+                            }),
+            })
     @PostMapping
     public ResponseEntity<ResponseScheduledInboundOrderDTO> createScheduledInboundOrder(@RequestBody @Valid ScheduledInboundOrderDTO dto) {
 
@@ -42,6 +61,18 @@ public class ScheduledInboundOrderController {
         return scheduledInboundOrderAssembler.toResponse(saved, HttpStatus.CREATED, null);
     }
 
+    @Operation(summary = "LISTA O SCHEDULED INBOUND ORDER REQUISITADO", description = "Lista o Scheduled Inbound Order requisitado")
+    @ApiResponses(
+            value = {
+                    @ApiResponse(
+                            responseCode = "200",
+                            description = "Encontrado",
+                            content = {
+                                    @Content(
+                                            mediaType = "application/json",
+                                            schema = @Schema(implementation = ResponseScheduledInboundOrderDTO.class))
+                            }),
+            })
     @GetMapping("{id}")
     public ResponseEntity<ResponseScheduledInboundOrderDTO> findById(@PathVariable Long id) {
         ScheduledInboundOrder response = scheduledInboundOrderService.findById(id);
@@ -49,6 +80,18 @@ public class ScheduledInboundOrderController {
         return scheduledInboundOrderAssembler.toResponse(response, HttpStatus.OK, null);
     }
 
+    @Operation(summary = "LISTA TODOS SCHEDULED INBOUND ORDERS", description = "Lista todos Scheduled Inbound Order requisitados")
+    @ApiResponses(
+            value = {
+                    @ApiResponse(
+                            responseCode = "200",
+                            description = "Encontrado",
+                            content = {
+                                    @Content(
+                                            mediaType = "application/json",
+                                            schema = @Schema(implementation = ResponseScheduledInboundOrderDTO.class))
+                            }),
+            })
     @GetMapping
     public ResponseEntity<List<ResponseScheduledInboundOrderDTO>> findAll(
             @RequestParam(required = false) @DateTimeFormat(iso = DateTimeFormat.ISO.DATE) LocalDate date) {
@@ -65,6 +108,18 @@ public class ScheduledInboundOrderController {
         return scheduledInboundOrderAssembler.toResponse(response, HttpStatus.OK, null);
     }
 
+    @Operation(summary = "ATUALIZA O SCHEDULED INBOUND ORDER REQUISITADO", description = "Atualiza o Scheduled Inbound Order requisitado")
+    @ApiResponses(
+            value = {
+                    @ApiResponse(
+                            responseCode = "201",
+                            description = "Criado",
+                            content = {
+                                    @Content(
+                                            mediaType = "application/json",
+                                            schema = @Schema(implementation = ResponseScheduledInboundOrderDTO.class))
+                            }),
+            })
     @PutMapping("{id}")
     @JsonView(ScheduledInboundOrderView.update.class)
     public ResponseEntity<ResponseScheduledInboundOrderDTO> update(@RequestBody @Valid ScheduledInboundOrderDTO dto, @PathVariable Long id) {
@@ -77,6 +132,18 @@ public class ScheduledInboundOrderController {
         return scheduledInboundOrderAssembler.toResponse(saved, HttpStatus.CREATED, null);
     }
 
+    @Operation(summary = "REMOVE O SCHEDULED INBOUND ORDER REQUISITADO", description = "Remove o Scheduled Inbound Order requisitado")
+    @ApiResponses(
+            value = {
+                    @ApiResponse(
+                            responseCode = "204",
+                            description = "Deletado com sucesso",
+                            content = {
+                                    @Content(
+                                            mediaType = "application/json",
+                                            schema = @Schema(implementation = void.class))
+                            }),
+            })
     @DeleteMapping("{id}")
     public ResponseEntity<?> delete(@PathVariable Long id) {
         scheduledInboundOrderService.deleteScheduledInboundOrder(id);
